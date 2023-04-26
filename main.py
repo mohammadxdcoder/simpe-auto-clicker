79ed8a97ee1cc44ac0f84232feef75138ca17e4e
Mohammad belal <122786375+mohammadxdcoder@users.noreply.github.com>
2023-04-26 18:22:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pyautogui as gui
 import keyboard as kb
 import time as t
 import sys
-cps = int(input("Enter cps"))
-key = input("Enter key input and key output")
+cps = int(input("Enter cps: "))
+key = input("Enter key input and key output: ")
 if len(key) >= 2:
     while True:
         if kb.is_pressed(key[0]) == True:
