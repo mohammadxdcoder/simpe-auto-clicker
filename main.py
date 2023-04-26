28951541485440eb6085d17eec996c40b9690c15
Mohammad belal <122786375+mohammadxdcoder@users.noreply.github.com>
2023-04-26 18:21:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import pyautogui as gui
 import keyboard as kb
 import time as t
 import sys
+cps = int(input("Enter cps"))
 key = input("Enter key input and key output")
 if len(key) >= 2:
     while True:
         if kb.is_pressed(key[0]) == True:
-            gui.press(key[1])
+            for x in range(cps):
+                gui.press(key[1])
+                t.sleep(1 / cps)
         else: continue
 else:
     sys.exit()
