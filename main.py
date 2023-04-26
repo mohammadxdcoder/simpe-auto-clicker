68574c46e154dcedcf3a1b626fc18e889e843be2
Mohammad belal <122786375+mohammadxdcoder@users.noreply.github.com>
2023-04-26 18:12:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-
+import pyautogui as gui
+import keyboard as kb
+import time as t
+import sys
+key = input("Enter key input and key output")
+if len(key) >= 2:
+    while True:
+        if kb.is_pressed(key[0]) == True:
+            gui.press(key[1])
+        else: continue
+else:
+    sys.exit()
