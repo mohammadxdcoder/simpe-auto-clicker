e49559909aab0220c896b2767438d78ab8854a5b
Mohammad belal <122786375+mohammadxdcoder@users.noreply.github.com>
2023-04-27 19:53:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import pyautogui as gui
 import keyboard as kb
-import time as t
-import sys
-cps = int(input("Enter cps: "))
-key = input("Enter key input and key output: ")
-if len(key) >= 2:
-    while True:
-        if kb.is_pressed(key[0]) == True:
-            for x in range(cps):
-                gui.press(key[1])
-        elif kb.is_pressed("q") == True:
-            sys.exit()
-                
-        else: continue
+print("Enter cps as number and enter keys as [keyinput-nospace-keyout]: ")
+try:
+    cps = int(input("Enter cps: "))
+    keys = input("Enter keys as [keyinput-nospace-keyout]: ")
+except:
+    exit()
+if len(str(cps)) == 0 | len(keys) > 2 | len(keys) == 0:
+    print("Exiting...")
+    exit()
 else:
-    sys.exit()
+    while True:
+        if kb.is_pressed(keys[0]) == True:
+            for c in cps:
+                kb.send(keys[1])
+        
+        else:
+            if kb.is_pressed("q") == True:
+                exit()
+            continue
